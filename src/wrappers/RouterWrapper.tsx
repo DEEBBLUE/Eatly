698b765aaafc52b51e1e7a76c087a0b04d2bee11
Lifter <Sosite.my_dick@gmail.com>
2025-05-28 17:04:15 -0400

    Bugfix: adding destuctarition

--- src/wrappers/RouterWrapper.tsx
+++ src/wrappers/RouterWrapper.tsx
@@ -2,7 +2,8 @@ import type { FC } from "react";
 import type { IRouterPath } from "../types/IRouterPath";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const RouterWrapper: FC<IRouterPath[]> = (list) => {
+const RouterWrapper: FC<IRouterPath[]> = ({ list }) => {
+  
   return(
     <BrowserRouter>
       <Routes>
