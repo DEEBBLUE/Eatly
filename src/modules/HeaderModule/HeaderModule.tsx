89c6685ddfe1129ad604dbe97fb931d983ed2f0f
Lifter <Sosite.my_dick@gmail.com>
2025-05-28 17:47:28 -0400

    Add navbar comonent in HeaderModule

--- src/modules/HeaderModule/HeaderModule.tsx
+++ src/modules/HeaderModule/HeaderModule.tsx
@@ -1,12 +1,12 @@
 import type { FC } from "react";
-
+import NavBar from "./components/NavBar/NavBar";
+import { NavBarList } from "./NavBarList";
 
 const HeaderModule: FC = () => {
 
   return(
     <>
-
-
+      <NavBar list={NavBarList}/> 
     </>
   )
 }
