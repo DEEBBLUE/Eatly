2a55ce68949ac9a2c2129a9c677dccbf1fd842fd
Lifter <Sosite.my_dick@gmail.com>
2025-05-28 16:46:18 -0400

    Create wrappers for routing

---
+++ src/wrappers/RouterWrapper.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+import type { IRouterPath } from "../types/IRouterPath";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const RouterWrapper: FC<IRouterPath[]> = (list) => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        {
+          list.map((pair) => <Route path={pair.path} element={pair.element}>)
+        }
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
