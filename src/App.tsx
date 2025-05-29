f91474607c67280a8db87a7b741ea53e87814b2f
Lifter <Sosite.my_dick@gmail.com>
2025-05-29 07:35:30 -0400

    Adding basic stylesheet file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react"
 import RouterWrapper from "./wrappers/RouterWrapper"
 import { RoutingList } from "./routingList"
+import './index.scss'
 
 
 const App: FC = () => {
