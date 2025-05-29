299b15c8091d347ba31d74c810e795040c23535e
Lifter <Sosite.my_dick@gmail.com>
2025-05-29 09:24:00 -0400

    Create and add styles in module

--- src/modules/HeaderModule/HeaderModule.tsx
+++ src/modules/HeaderModule/HeaderModule.tsx
@@ -1,13 +1,13 @@
 import type { FC } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import { NavBarList } from "./NavBarList";
+import "./style.scss"
 
 const HeaderModule: FC = () => {
-
   return(
-    <>
+    <header>
       <NavBar list={NavBarList}/> 
-    </>
+    </header>
   )
 }
 
