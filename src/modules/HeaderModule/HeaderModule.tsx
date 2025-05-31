815c1ce7f989039352682a2e2011dd6f35b727f0
Lifter <Sosite.my_dick@gmail.com>
2025-05-31 14:28:36 -0400

    Add logo component

--- src/modules/HeaderModule/HeaderModule.tsx
+++ src/modules/HeaderModule/HeaderModule.tsx
@@ -2,12 +2,13 @@ import type { FC } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import { NavBarList } from "./NavBarList";
 import "./style.scss"
+import Logo from "./components/Logo/Logo";
 
 const HeaderModule: FC = () => {
   return(
     <header className="header">
       <div className="header__menuContainer">
-         
+        <Logo/>
         <NavBar list={NavBarList}/> 
       </div>
     </header>
