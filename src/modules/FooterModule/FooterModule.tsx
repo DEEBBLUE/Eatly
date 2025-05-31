2c8fd5f931b514a36314d3f9691ea08c1ca75f74
Lifter <Sosite.my_dick@gmail.com>
2025-05-31 12:26:47 -0400

    Add social media list

--- src/modules/FooterModule/FooterModule.tsx
+++ src/modules/FooterModule/FooterModule.tsx
@@ -1,9 +1,22 @@
 import type { FC } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Logo from "../../components/Logo/Logo";
 import "./style.scss";
+import SheetList from "./components/SheetList/SheetList";
 
 const FooterModule: FC = () => {
   return(
-    <></>
+    <footer className="footer">
+      <header className="footer__header">
+        <Logo style="footer__logo"/> 
+        <NavBar styles="footer__navbar"/>
+      </header>
+      <hr className="footer__hr"/>
+      <div className="footer__sheetContainer">
+        <p className="footer__sheetContainer-p">&copy; 2023 EATLY All Rights Reserved</p>
+        <SheetList/>
+      </div>
+    </footer>
   )
 } 
 
