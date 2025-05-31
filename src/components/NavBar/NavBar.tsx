50fafe8494efee7f41d442045e232be0779c731f
Lifter <Sosite.my_dick@gmail.com>
2025-05-31 11:27:05 -0400

    Create styles for footer

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,9 +2,15 @@ import type { FC } from "react";
 import "./navbar.scss"
 import { NavBarList } from "./NavBarList.ts"
 
-const NavBar: FC= () => {
+type navBarStyles = "header__navbar" | "footer__navbar"
+
+interface NavBarProps{
+  styles: navBarStyles 
+}
+
+const NavBar: FC<NavBarProps> = ({ styles }) => {
   return(
-    <nav className="header__navbar">
+    <nav className={"navbar" + styles}>
       <ul>
         {
           NavBarList.map((item) => <li>{item[0]}</li> )
