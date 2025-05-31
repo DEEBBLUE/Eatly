bb13aeb5d757fe1dd17b908d81d835d37932f209
Lifter <Sosite.my_dick@gmail.com>
2025-05-31 09:10:25 -0400

    Add Auth components

--- src/modules/HeaderModule/HeaderModule.tsx
+++ src/modules/HeaderModule/HeaderModule.tsx
@@ -1,16 +1,23 @@
 import type { FC } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import { NavBarList } from "./NavBarList";
-import "./style.scss"
+import "./style.scss";
 import Logo from "./components/Logo/Logo";
+import Auth from "./components/Auth/Auth";
 
 const HeaderModule: FC = () => {
   return(
     <header className="header">
       <div className="header__menuContainer">
-        <Logo/>
-        <NavBar list={NavBarList}/> 
+        <div className="header__menuContainer-left">
+          <Logo/>
+          <NavBar list={NavBarList}/>
+        </div>
+        <div className="header__menuContainer-rigth">
+          <Auth/>
+        </div>
       </div>
+      <hr className="header__hr"/>
     </header>
   )
 }
