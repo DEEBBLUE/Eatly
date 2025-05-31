fea93f046b1248af033d4554ec0199e569b3cedb
Lifter <Sosite.my_dick@gmail.com>
2025-05-31 09:09:25 -0400

    Create global components button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+import "./button.scss";
+
+interface ButtonProps{
+  styles?: string,
+  name: string,
+  action: () => void
+}
+
+const Button: FC<ButtonProps> = ({ styles="btn_style",name,action }) => {
+  return(
+    <button className={"btn " + styles} onClick={action}>
+      {name}
+    </button>
+  )
+} 
+export default Button;
